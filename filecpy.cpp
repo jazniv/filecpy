65e739acc1341ee863d1b8911010679f06a3f369
garrettbolen <59625632+garrettbolen@users.noreply.github.com>
2022-02-20 15:51:32 -0800

    Update filecpy.cpp

--- filecpy.cpp
+++ filecpy.cpp
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <cstdlib>
+#include <string>
+#include <fcntl.h>
+#include <unistd.h>
+
+using namespace std;
 
 int main (int argc, char *argv[])
 {
@@ -8,19 +14,19 @@ int main (int argc, char *argv[])
     exit(1);
   };
 
-  String input = argv[1];
-  String output = argv[2];
+  std::string input = argv[1];
+  std::string output = argv[2];
 
-  int inputCheck = open(input);
-  int outputCheck = open(output);
+  int inFD = open(input.c_str(), O_RDONLY);
+  int outFD = open(output.c_str(), O_RDONLY);
 
-  if(inputCheck == -1){
-    printf("Input file not found.\n")
+  if(inFD == -1){
+    printf("Input file not found.\n");
     exit(1);
   };
 
-  if(outputCheck == -1){
-    printf("Output file not found.\n")
+  if(outFD == -1){
+    printf("Output file not found.\n");
   };
 
 
