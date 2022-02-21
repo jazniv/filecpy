0316929c999d15abcf70197cb43540e147f57045
garrettbolen <59625632+garrettbolen@users.noreply.github.com>
2022-02-20 16:27:56 -0800

    Update filecpy.cpp

--- filecpy.cpp
+++ filecpy.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 using namespace std;
 
@@ -12,23 +13,35 @@ int main (int argc, char *argv[])
   if(argc < 3){
     printf("Invalid number of arguments!\n");
     exit(1);
-  };
+  }
 
   std::string input = argv[1];
   std::string output = argv[2];
 
   int inFD = open(input.c_str(), O_RDONLY);
-  int outFD = open(output.c_str(), O_RDONLY);
+  int outFD = open(output.c_str(), O_WRONLY | O_CREAT);
 
   if(inFD == -1){
     printf("Input file not found.\n");
     exit(1);
-  };
+  }
 
   if(outFD == -1){
     printf("Output file not found.\n");
-  };
+    exit(1);
+  }
 
+  struct stat st;
+  stat(input.c_str(), &st);
+  int size = st.st_size;
+
+  char buf[size];
+
+  read(inFD, buf, size);
+  write(outFD, buf, size);
+
+  close(inFD);
+  close(outFD);
 
 
   return 0;
