c301cd85ebd0b5f960aaad95098050f9126baade
jazniv <glodek@chapman.edu>
2022-02-20 16:38:45 -0800

    added error handling

--- filecpy.cpp
+++ filecpy.cpp
@@ -10,39 +10,38 @@ using namespace std;
 int main (int argc, char *argv[])
 {
 
-  if(argc < 3){
-    printf("Invalid number of arguments!\n");
-    exit(1);
+  try {
+
+    if (argc < 3)
+      throw(-1);
+
+    std::string input = argv[1];
+    std::string output = argv[2];
+
+    int inFD = open(input.c_str(), O_RDONLY);
+    int outFD = open(output.c_str(), O_WRONLY | O_CREAT);
+
+    if ((inFD == -1) || (outFD == -1))
+      throw (-1);
+
+    struct stat st;
+    stat(input.c_str(), &st);
+    int size = st.st_size;
+
+    char buf[size];
+
+    read(inFD, buf, size);
+    write(outFD, buf, size);
+
+    close(inFD);
+    close(outFD);
+
   }
 
-  std::string input = argv[1];
-  std::string output = argv[2];
-
-  int inFD = open(input.c_str(), O_RDONLY);
-  int outFD = open(output.c_str(), O_WRONLY | O_CREAT);
-
-  if(inFD == -1){
-    printf("Input file not found.\n");
+  catch (int err) {
+    printf("Invalid input.\n");
     exit(1);
   }
-
-  if(outFD == -1){
-    printf("Output file not found.\n");
-    exit(1);
-  }
-
-  struct stat st;
-  stat(input.c_str(), &st);
-  int size = st.st_size;
-
-  char buf[size];
-
-  read(inFD, buf, size);
-  write(outFD, buf, size);
-
-  close(inFD);
-  close(outFD);
-
 
   return 0;
 }
